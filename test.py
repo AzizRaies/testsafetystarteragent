cc6e79c34db499b29acdbaaede1443b81fedd319
AzizRaies <76903786+AzizRaies@users.noreply.github.com>
2021-01-03 14:08:37 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,52 @@
+import gym
+import open_safety_gym
+import safety_gym
+from mpi4py import MPI
+import safe_rl
+from safe_rl import cpo
+from safe_rl import trpo
+from safe_rl import trpo_lagrangian
+from matplotlib import pyplot as plt
+import pickle
+
+
+
+all_envs = gym.envs.registry
+#env = gym.make('SafetyHopperRun-v0')
+env = gym.make('SafetyHumanoidCircle-v0')
+#env = gym.make('SafetyAntCircle-v0') #epcost=0
+#env = gym.make('SafetyBallReach-v2')
+
+
+
+#cpo(env_fn = lambda : env,epochs=150,ac_kwargs=dict(hidden_sizes=(64, 32)))
+trpo_lagrangian(env_fn = lambda : env,epochs=10,ac_kwargs=dict(hidden_sizes=(64, 32)))
+
+
+
+#X_cpo = pickle.load(open('CPOAgentaverage_returns_and_Costs_per_epochSafetyBallCircle-v0.sav', 'rb'))
+#X_trpo = pickle.load(open('TRPOAgent_average_returns_and_Costs_per_epochSafetyBallCircle-v0.sav', 'rb'))
+#
+#
+#plt.figure(1)
+#plt.plot(X_cpo[0],label='cpo')
+
+#plt.plot(X_trpo[0],label='trpo_lagrangian')
+#plt.legend()
+#plt.savefig('EpRetSafetyBallCircle_v2')
+
+#plt.figure(2)
+
+#plt.plot(X_cpo[1],label='cpo')
+
+#plt.plot(X_trpo[1],label='trpo_lagrangian')
+#plt.legend()
+#plt.savefig('SafetyBallCircle-v2')
+
+#plt.figure(4)
+
+#plt.plot(X_cpo[1],label='EpCost')
+
+
+#plt.legend()
+#plt.savefig('CPOAgent_average_Costs_per_epochSafetyBallCircle-v0')

2cbe304175e757d0fdb66b93dfa88a722dda7883
AzizRaies <76903786+AzizRaies@users.noreply.github.com>
2021-01-16 12:24:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,10 @@ setup(
     name='safe_rl',
     packages=['safe_rl'],
     install_requires=[
-        'gym~=0.15.3',
+        
         'joblib==0.14.0',
         'matplotlib==3.1.1',
         'mpi4py==3.0.2',
-        'numpy~=1.17.4',
         'seaborn==0.8.1',
         'tensorflow==1.13.1',
     ],

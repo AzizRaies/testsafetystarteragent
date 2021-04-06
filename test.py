97f6ac6bd848f39b4f2c84872bb6cbd4bed30e82
AzizRaies <76903786+AzizRaies@users.noreply.github.com>
2021-04-06 18:44:23 +0200

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ env = gym.make('SafetyBallReach-v0')
 
 
 #cpo(env_fn = lambda : env,FileName="CPO_Seed4_new", seed = 0 ,alpha = 0,epochs=150,cost_lim=25,ac_kwargs=dict(hidden_sizes=(64, 32)))
-trpo_lagrangian(env_fn = lambda : env,FileName="TRPO_Seed2_new_one_signal_alpha_0_5",alpha =0.5 ,epochs=150,cost_lim=10,seed =2,ac_kwargs=dict(hidden_sizes=(64, 32)))
+trpo_lagrangian(env_fn = lambda : env,FileName="TRPO_Seed3_new_one_signal_alpha_0_5",alpha =0.5 ,epochs=150,cost_lim=10,seed =3,ac_kwargs=dict(hidden_sizes=(64, 32)))
 
 
 
